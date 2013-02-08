0aa603a2eb8e97be5cd45fd80f20e5d9b483d870
Jono Cooper <jono@jonocooper.com>
2013-02-09 07:19:45 +1300

    Refactor

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,8 @@
+/*jshint asi: true*/
 window.app = "chrome"
+
+var stormcloud = {},
+	slider = new Swipe(document.getElementById('container'))
 
 $(function() {
 	// Copy Ze localstorage over
@@ -12,31 +16,38 @@ $(function() {
 		} else {
 			slider.next()
 		}
+		stormcloud.posChange()
 	})
 })
 
-stormcloud = {
-	reload: function() {
+stormcloud.reload = function() {
 
-		var template = Handlebars.templates['pane.template'],
-			weather = JSON.parse(localStorage.stormcloud_weathercache)
+	var template = Handlebars.templates['pane.template'],
+		weather = JSON.parse(localStorage.stormcloud_weathercache)
 
-		$('#container ul').html("")
+	$('#container ul').html("")
 
-		for (var i in weather) {
-			$("#container ul").append("<li style='display: none'><div></div></li>")
-			$("#container li > div")[i].innerHTML = template(weather[i])
-		}
-
-		slider = new Swipe(document.getElementById('container'))
-		stormcloud.textfix()
-
-	},
-
-	textfix: function() {
-		$(".city").fitText(0.29)
-		$(".left").fitText(0.17)
-		$(".right").fitText(0.5)
-		$(".forecast span.day, .forecast span.temp").fitText(0.17)
+	for (var i in weather) {
+		$("#container ul").append("<li style='display: none'><div></div></li>")
+		$("#container li > div")[i].innerHTML = template(weather[i])
 	}
+
+	slider = new Swipe(document.getElementById('container'))
+	stormcloud.textfix()
+	stormcloud.posChange()
+}
+
+stormcloud.posChange = function() {
+	if (localStorage.color == "gradient") {
+		$('#container').css('background-color', $($('.middle')[slider.getPos()]).attr("data-background"))
+	} else {
+		$('#container').css('background-color', localStorage.color)
+	}
+}
+
+stormcloud.textfix = function() {
+	$(".city").fitText(0.29)
+	$(".left").fitText(0.17)
+	$(".right").fitText(0.5)
+	$(".forecast span.day, .forecast span.temp").fitText(0.17)
 }
