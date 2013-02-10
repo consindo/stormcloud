13a19750ceab2941a52f9b06e37690ec4772bf2f
Jono Cooper <jono@jonocooper.com>
2013-02-11 11:21:35 +1300

    Add Appearance Settings

--- js/templates/settings.js
+++ js/templates/settings.js
@@ -34,7 +34,17 @@ function program5(depth0,data) {
   foundHelper = helpers.appearanceText;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.appearanceText; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  buffer += escapeExpression(stack1) + "</h3>\n  <div>\n    <div class=\"content\">\n      <p>This example simply sets a class attribute to the details and let's an\n      external stylesheet toggle the collapsed state.</p>\n      <p>Hello Sir.</p>\n    </div>\n  </div>\n  <h3>";
+  buffer += escapeExpression(stack1) + "</h3>\n  <div>\n    <div class=\"content\">\n      <div class=\"toggleswitch measurement\">\n        ";
+  foundHelper = helpers.measurement;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
+  else { stack1 = depth0.measurement; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n      </div>\n      <div class=\"toggleswitch speed\">\n          ";
+  foundHelper = helpers.speed;
+  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
+  else { stack1 = depth0.speed; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n      </div>\n      <div class=\"color\">\n          <span data-color=\"gradient\"></span>\n          <span data-color=\"A200FF\"></span>\n          <span data-color=\"FF0097\"></span>\n          <span data-color=\"00ABA9\"></span>\n          <span data-color=\"8CBF26\"></span>\n          <span data-color=\"A05000\"></span>\n          <span data-color=\"333333\"></span>\n          <span data-color=\"F09609\"></span>\n          <span data-color=\"1BA1E2\"></span>\n          <span data-color=\"E51400\"></span>\n          <span data-color=\"339933\"></span>\n      </div>\n    </div>\n  </div>\n  <h3>";
   foundHelper = helpers.otherText;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.otherText; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
@@ -50,17 +60,7 @@ function program5(depth0,data) {
   stack1 = depth0.location;
   stack1 = helpers.each.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(1, program1, data)});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n    </ul>\n</div>\n<div class=\"wrapperTwenty\">\n    <div class=\"toggleswitch measurement\">\n        ";
-  foundHelper = helpers.measurement;
-  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
-  else { stack1 = depth0.measurement; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n    </div>\n    <div class=\"toggleswitch speed\">\n        ";
-  foundHelper = helpers.speed;
-  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
-  else { stack1 = depth0.speed; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n    </div>\n    <div class=\"color\">\n        <span data-color=\"gradient\"></span>\n        <span data-color=\"A200FF\"></span>\n        <span data-color=\"FF0097\"></span>\n        <span data-color=\"00ABA9\"></span>\n        <span data-color=\"8CBF26\"></span>\n        <span data-color=\"A05000\"></span>\n        <span data-color=\"333333\"></span>\n        <span data-color=\"F09609\"></span>\n        <span data-color=\"1BA1E2\"></span>\n        <span data-color=\"E51400\"></span>\n        <span data-color=\"339933\"></span>\n    </div>\n    <div class=\"launcher\" data-boop=\"";
+  buffer += "\n    </ul>\n</div>\n<div class=\"wrapperTwenty\">\n    <div class=\"launcher\" data-boop=\"";
   foundHelper = helpers.launcher;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }
   else { stack1 = depth0.launcher; stack1 = typeof stack1 === functionType ? stack1() : stack1; }
